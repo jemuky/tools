932d6b7a4f4df86450196b595db6e7a2abc23b77
jemuky <jymkyu@outlook.com>
2024-10-18 16:06:33 +0800

    微信不支持网页端登录，暂时不能用，有时间再看看桌面端的

--- wechat_tools/Program.cs
+++ wechat_tools/Program.cs
@@ -1,12 +1,25 @@
 // See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
 
-Stopwatch watch = new();
+// 配置 Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day) // 写入文件
+    .CreateLogger();
+
+
+Log.Debug("Hello, World!");
+// 注册编码提供程序，否则某些响应貌似返回gbk，导致ReadAsStringAsync失败
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+// Stopwatch watch = new();
 // 创建主机
 var host = Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((ctx, config) => {
     // 移除默认的 appsettings.json
@@ -21,10 +34,26 @@ var configuration = host.Services.GetRequiredService<IConfiguration>();
 // 访问配置项
 var setting1 = configuration["Logging:LogLevel:Microsoft"];
 var setting2 = configuration["ConnectionStrings:DefaultConnection"];
-Console.WriteLine($"Setting1: {setting1}, setting2: {setting2}");
+Log.Debug($"Setting1: {setting1}, setting2: {setting2}");
 
+// 当前时间秒值
 static long curTime() {
     return (long)(DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalSeconds;
+}
+
+static Dictionary<string, string> decodeWXRsp(string rspBody) {
+    Dictionary<string, string> result = [];
+    var regex = new Regex(@"([a-zA-Z_][a-zA-Z0-9_.]*)\s*=\s*(['\""]?)(.*?)\2(?=\s*;|$)", RegexOptions.Singleline);
+    var matches = regex.Matches(rspBody);
+    foreach (Match match in matches) {
+        if (match.Groups.Count == 4) {
+            string key = match.Groups[1].Value;
+            string value = match.Groups[3].Value;
+
+            result[key] = value;
+        }
+    }
+    return result;
 }
 
 // 参见 https://www.cnblogs.com/chenxizhaolu/p/8426286.html
@@ -35,7 +64,7 @@ using (HttpClient client = new()) {
     var rspBody = await rsp.Content.ReadAsStringAsync();
 
     var cookie = string.Join(',', rsp.Headers.GetValues("Set-Cookie"));
-    Console.WriteLine($"rspBody: html没必要打印，长度: {rspBody.Length}, cookie: {cookie}");
+    Log.Information($"1. cookie: rspBody: html没必要打印，长度: {rspBody.Length}, cookie: {cookie}");
 
     client.DefaultRequestHeaders.Add("session", "");
     client.DefaultRequestHeaders.Add("cookie", cookie);
@@ -44,35 +73,65 @@ using (HttpClient client = new()) {
     rsp = await client.GetAsync($"https://wx.qq.com/jslogin?appid=wx782c26e4c19acffb&fun=new&lang=en_US&_={curTime()}&redirect_uri=https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxnewloginpage");
     rsp.EnsureSuccessStatusCode();
     rspBody = await rsp.Content.ReadAsStringAsync();
-    Console.WriteLine($"rspBody: {rspBody}, headers: {rsp.Headers}");
+    Log.Information($"2. 二维码uuid: rspBody: {rspBody}, headers: {rsp.Headers}");
 
-    string? uuid = null;
-    foreach (var item in rspBody.Split(";")) {
-        var v = item.Trim();
-        if (v.StartsWith("window.QRLogin.uuid")) {
-            uuid = v[(v.IndexOf('=') + 1)..v.Length].Trim()[1..^1];
-        }
-    }
-    if (uuid == null) {
-        Console.WriteLine("没获取到uuid");
+    if (!decodeWXRsp(rspBody).TryGetValue("window.QRLogin.uuid", out string? uuid)) {
+        Log.Error($"没获取到uuid, rsp={rspBody}");
         return;
     }
-    Console.WriteLine($"获取到了uuid({uuid})");
+    Log.Information($"获取到了uuid({uuid})");
 
     // 3. 下载和展示二维码
     rsp = await client.GetAsync($"https://wx.qq.com/qrcode/{uuid}");
     rsp.EnsureSuccessStatusCode();
     byte[] qrcode = await rsp.Content.ReadAsByteArrayAsync();
-    Console.WriteLine($"rspBody: {qrcode.Length}, headers: {rsp.Headers}");
-    await File.WriteAllBytesAsync("qrcode.jpg", qrcode);
+    Log.Information($"3. 二维码: rspBody: {qrcode.Length}, headers: {rsp.Headers}");
+    // 二维码
+    try {
+        await File.WriteAllBytesAsync("qrcode.jpg", qrcode);
+    } catch (Exception e) {
+        Log.Error($"write qrcode failed, e={e}");
+        return;
+    }
 
     // 4. 扫码和确认
     var now = curTime();
-    rsp = await client.GetAsync($"https://wx.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid={uuid}&r={now / 1524}&_={now}&tip=0");
+    rsp = await client.GetAsync($"https://wx.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid={uuid}&r={now / 1524}&_={now}");
     rsp.EnsureSuccessStatusCode();
     rspBody = await rsp.Content.ReadAsStringAsync();
-    // rspBody: window.code=201;window.userAvatar = 'xxxxx';(如果返回200还有redirect_uri)
-    Console.WriteLine($"4. 扫码和确认: rspBody: {rspBody}, headers: {rsp.Headers}");
+    // rspBody: window.code=201;window.userAvatar = 'xxxxx';
+    Console.WriteLine($"4. 扫码: headers: {rsp.Headers}");
+    if (!decodeWXRsp(rspBody).TryGetValue("window.userAvatar", out string? userAvatar)) {
+        Log.Error($"没获取到头像信息, rsp={rspBody}");
+        return;
+    }
+    try {
+        await File.WriteAllBytesAsync("user_avatar.jpg", Convert.FromBase64String(userAvatar.Replace("data:img/jpg;base64,", null)));
+    } catch (Exception e) {
+        Log.Error($"write userAvatar failed, userAvatar={userAvatar}, e={e}");
+        return;
+    }
+
+    // 5. 再请求一次，用来确认登录
+    now = curTime();
+    rsp = await client.GetAsync($"https://wx.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid={uuid}&r={now / 1524}&_={now}");
+    rsp.EnsureSuccessStatusCode();
+    rspBody = await rsp.Content.ReadAsStringAsync();
+    // rspBody: window.code=200;window.redirect_uri = 'xxxxx';
+    Log.Information($"5. 确认扫码: rspBody: {rspBody}, headers: {rsp.Headers}");
+    if (!decodeWXRsp(rspBody).TryGetValue("window.redirect_uri", out string? redirectUri)) {
+        Log.Error($"没获取到window.redirect_uri, rsp={rspBody}");
+        return;
+    }
+
+    // 6. 初始化页面、获取登录信息
+    rsp = await client.GetAsync(redirectUri);
+    rsp.EnsureSuccessStatusCode();
+    rspBody = await rsp.Content.ReadAsStringAsync();
+    // rspBody: window.code=200;window.redirect_uri = 'xxxxx';
+    Log.Information($"6. 转发页面: rspBody: {rspBody}, headers: {rsp.Headers}");
 }
+
+await Log.CloseAndFlushAsync();
 
 // host.Run();
